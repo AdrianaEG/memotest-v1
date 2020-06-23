2f1425c53d88ef9dfed3c1d51fc8975a25c2ec94
AdrianaEG <adriana.ngc438@gmail.com>
2020-06-22 23:24:22 -0300

    cambios

--- src/main.js
+++ src/main.js
@@ -1,21 +1,49 @@
 $btnComenzarReiniciar = document.querySelector('#btn-comenzar-reiniciar');
 $btnComenzarReiniciar.addEventListener('click', comenzarOReiniciar);
+let arregloConImagenes = ['green', 'yellow', 'orange', 'blue', 'black', 'white', 'brown', 'violet', 'grey', 'pink','green', 'yellow', 'orange', 'blue', 'black', 'white', 'brown', 'violet', 'grey', 'pink'];
+
 function comenzarOReiniciar(){
     let cantidadIntentos = 0;
-    obtenerAleatorio();
-    permitirClickearTarjetas();
+    let $todasLasTarjetas = document.querySelector('main').querySelectorAll('div');//nodeList
+    console.log($todasLasTarjetas);
+    arregloConImagenes = desordenarArreglo(arregloConImagenes);
+    console.log(arregloConImagenes);
+    darImagenALosElementos($todasLasTarjetas, arregloConImagenes);
+    permitirClickearTarjetas($todasLasTarjetas, arregloConImagenes);
 }
 
-function permitirClickearTarjetas(){
-    let $todasLasTarjetas = document.querySelector('main').children;//Object
-    console.log($todasLasTarjetas.length);
-    for(let i=0; i<$todasLasTarjetas.length; i++){
-        $todasLasTarjetas[i].onclick = function(){
+let darImagenALosElementos = (elementos, arregloConClases)=>{
+    let i=0;
+   elementos.forEach(elemento => {
+       elemento.classList.add(arregloConClases[i]);
+       i++;
+   });
+}
 
-        }
+let darVueltaTarjeta = (elemento, claseElementoClickeado)=>{
+    elemento.style.background = claseElementoClickeado;
+}
+
+function permitirClickearTarjetas($tarjetas){
+    $tarjetas.forEach($tarjeta =>{
+        $tarjeta.onclick = function(e){
+            console.log(e.target);
+            let elementoClickeado = e.target;
+            let claseElementoClickeado = e.target.classList.value;
+            darVueltaTarjeta(elementoClickeado, claseElementoClickeado);
+        };
+    });
+}
+
+function desordenarArreglo(arreglo){
+    let aleatorio = 0;
+    let auxiliar = 0;
+    for(let i=0; i<arreglo.length; i++){
+        aleatorio = Math.floor(Math.random() * arreglo.length);
+        auxiliar = arreglo[aleatorio];
+
+        arreglo.splice(aleatorio, 1);
+        arreglo.push(auxiliar);
     }
-}
-
-function obtenerAleatorio(){
-    
+    return arreglo;
 }
