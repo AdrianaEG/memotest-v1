2b403d1674ae8ed55a7ac8f4be282578421d269c
AdrianaEG <adriana.ngc438@gmail.com>
2020-06-26 13:38:50 -0300

    Se bloquean tarjetas al clickear 2, se comprueba si hay coincidencia

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 $btnComenzarReiniciar = document.querySelector('#btn-comenzar-reiniciar');
 $btnComenzarReiniciar.addEventListener('click', comenzarOReiniciar);
 let arregloConImagenes = ['imagen-01', 'imagen-02', 'imagen-03', 'imagen-04', 'imagen-05', 'imagen-06', 'imagen-07', 'imagen-08', 'imagen-09', 'imagen-10','imagen-01', 'imagen-02', 'imagen-03', 'imagen-04', 'imagen-05', 'imagen-06', 'imagen-07', 'imagen-08', 'imagen-09', 'imagen-10'];
+let cantidadIntentos = 0;
+let exito = 0;
 
 function comenzarOReiniciar(){
-    let cantidadIntentos = 0;
+    document.querySelector('#btn-comenzar-reiniciar').innerHTML = 'Reiniciar';
     let $todasLasTarjetas = document.querySelector('main').querySelectorAll('div');//nodeList
-    console.log($todasLasTarjetas);
+    limpiarTodasLasTarjetas($todasLasTarjetas);
     arregloConImagenes = desordenarArreglo(arregloConImagenes);
-    console.log(arregloConImagenes);
     darImagenALosElementos($todasLasTarjetas, arregloConImagenes);
     permitirClickearTarjetas($todasLasTarjetas, arregloConImagenes);
+}
+
+let limpiarTodasLasTarjetas = $tarjetas =>{
+    $tarjetas.forEach($tarjeta =>{
+        $tarjeta.classList = '';
+        $tarjeta.style.backgroundImage = '';
+    });
 }
 
 let darImagenALosElementos = (elementos, arregloConClases)=>{
@@ -21,20 +29,67 @@ let darImagenALosElementos = (elementos, arregloConClases)=>{
 }
 
 let darVueltaTarjeta = (elemento, claseElementoClickeado)=>{
-    //elemento.style.backgroundImage = 'url(img/imagen-01.jpg)'; si anda
-    //elemento.style.backgroundImage = `/memotest-v1/img${claseElementoClickeado}.jpg`;//claseElementoClickeado;no anda
     elemento.style.backgroundImage = `url(img/${claseElementoClickeado}.jpg)`;
 }
 
 function permitirClickearTarjetas($tarjetas){
+    let clicks = 0;
+    let elementosClickeados = [];
     $tarjetas.forEach($tarjeta =>{
         $tarjeta.onclick = function(e){
-            console.log(e.target);
             let elementoClickeado = e.target;
+            cantidadIntentos++;
+            bloquearElementoClickeado(elementoClickeado);
+            elementosClickeados.push(elementoClickeado);
             let claseElementoClickeado = e.target.classList.value;
             darVueltaTarjeta(elementoClickeado, claseElementoClickeado);
+            console.log('cantidad de clicks ' + clicks);
+            clicks++;
+            if(clicks == 2){
+                bloquearTarjetas($tarjetas);
+                console.log(hayCoincidencia(elementosClickeados));
+                if(!hayCoincidencia(elementosClickeados)){
+                    setTimeout(function(){
+                        quitarImagen(elementosClickeados);
+                        permitirClickearTarjetas($tarjetas);
+                    }, 600)
+                }
+                else{
+                    exito++;
+                    setTimeout(()=>{
+                        permitirClickearTarjetas($tarjetas)
+                    },600);
+                }
+            }
+            console.log(`los aciertos: ${exito}`);
+            console.log(`la cantidad de intentos que lleva ${cantidadIntentos/2}`);
         };
     });
+}
+
+function bloquearElementoClickeado(elemento){
+    elemento.onclick = function(){
+
+    }
+}
+
+function bloquearTarjetas($tarjetas){
+    $tarjetas.forEach($tarjeta=>{
+        $tarjeta.onclick = function(){
+
+        }
+    })
+}
+
+function quitarImagen(elementos){
+    for(let i=0; i<elementos.length; i++){
+        elementos[i].style.backgroundImage = '';
+        //elementos[i].classList = '';
+    }
+}
+
+function hayCoincidencia(elementos){
+    return elementos[0].classList[0] == elementos[1].classList[0];
 }
 
 function desordenarArreglo(arreglo){
